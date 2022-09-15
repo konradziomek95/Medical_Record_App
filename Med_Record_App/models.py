6a731340dbeb9ab4d5e3fe66a00ce743c78a3273
konradziomek95 <konrad.ziomek.95@gmail.com>
2022-09-15 15:21:38 +0200

    New Models

--- Med_Record_App/models.py
+++ Med_Record_App/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+PROFESSION = (
+    (1, 'Doctor'),
+    (2, 'Nurse'),
+    (3, 'Physiotherapist'),
+    (4, 'Dentist'),
+    (5, 'Paramedic'),
+
+)
+
+
+class MedUser(AbstractUser):
+    PWZ = models.CharField(max_length=10)
+    email = models.EmailField()
+    profession = models.CharField(choices=PROFESSION, max_length=64)
+
+
+class Patient(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    PESEL = models.CharField(max_length=11, unique=True)
+    date_of_birth = models.DateField()
+    address = models.TextField()
+    contact = models.TextField()
+
+
+class MedicalRecord(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    owner = models.ForeignKey(MedUser, on_delete=models.CASCADE)
+    diagnosis = models.CharField(max_length=128)
+    ICD_10 = models.CharField(max_length=128)
+    symptoms = models.TextField()
+    tests = models.TextField()
+    imaging_examination = models.BooleanField(default=False)
+    description_od_IE = models.TextField(null=True)
+    medicines = models.TextField(null=True)
+    medical_treatment = models.TextField(null=True)
+    home_recommendation = models.TextField(null=True)
+
+
+class Reservation(models.Model):
+    owner = models.ForeignKey(MedUser, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    time_of_reservation = models.DateTimeField()
+
+    class Meta:
+        unique_together = ('owner', 'time_of_reservation')
+
+
--- Project/settings.py
+++ Project/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-@j^wqj1&d68a=n^)!yt)7zv^d#1^gi!q1q#y#p8w((r@2#@!@@
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,17 +68,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Project.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'HOST': '127.0.0.1',
+        'NAME': 'med_app',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -100,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -112,7 +110,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
@@ -122,3 +119,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'Med_Record_App.MedUser'
